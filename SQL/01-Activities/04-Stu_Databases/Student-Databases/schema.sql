1cdf01eff5fa622e6c877ab1a7aa3aa75f849927
“Nayonna <Nayonna Purnell>
2022-12-04 23:19:55 -0500

    Created, Used, and Read the database

--- SQL/01-Activities/04-Stu_Databases/Student-Databases/schema.sql
+++ SQL/01-Activities/04-Stu_Databases/Student-Databases/schema.sql
@@ -1,1 +1,13 @@
 -- Write code here or enter directly in MySQL shell --
+
+-- Delete an existing Database --
+DROP DATABASE IF EXISTS sample_db;
+
+-- Create a new database --
+CREATE DATABASE sample_db;
+
+-- Select that database for use --
+USE sample_db;
+
+-- Read the database for use --
+SELECT DATABASE();
